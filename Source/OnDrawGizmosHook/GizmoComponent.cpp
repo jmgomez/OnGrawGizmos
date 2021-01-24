d06444a15a799c3ef742a613aacf17d28dfc9ceb
jmgomez <juamangomalv@gmail.com>
2021-01-24 11:21:24 +0000

    First Commit

---
+++ Source/OnDrawGizmosHook/GizmoComponent.cpp
@@ -0,0 +1,41 @@
+﻿#include "GizmoComponent.h"
+#include "UObject/SparseDelegate.h"
+
+void FGizmosComponentVisualizer::DrawVisualization(const UActorComponent* Component, const FSceneView* View,
+                                                   FPrimitiveDrawInterface* PDI) {
+	UActorComponent* ActorComponent = const_cast<UActorComponent*>(Component);
+	
+	UGizmoVisComponent* Comp = Cast<UGizmoVisComponent>(ActorComponent);	
+	if(Comp){		
+		Comp->SelectGizmo();						
+	}			
+}
+
+//
+// UGizmoVisComponent::UGizmoVisComponent() {	
+// 	// OnGizmosSelected.AddDynamic(this, &UGizmoVisComponent::TestFunction);
+// }
+
+void UGizmoVisComponent::SelectGizmo() {	
+	OnGizmosSelected.Broadcast();	
+
+	bool bImplementsIActorVis = GetOwner()->Implements<UDrawGizmo>();	
+	if (bImplementsIActorVis){	
+		IDrawGizmo::Execute_OnDrawGizmo(GetOwner());		
+	} else {
+		GEngine->AddOnScreenDebugMessage(-1,0, FColor::Red, TEXT("The interface '' is not implemented. "));
+	}
+}
+
+
+
+// void UGizmoVisComponent::SelectGizmo() const {
+// 	GEngine->AddOnScreenDebugMessage(-1,0, FColor::Orange, TEXT("DRAW VIS"));
+// 	bool bImplementsIActorVis = GetOwner()->Implements<UActorVisualizerInterface>();	
+// 	if (bImplementsIActorVis){
+// 		GEngine->AddOnScreenDebugMessage(-1,0, FColor::Orange, TEXT("DRAW VIS"));
+// 		IActorVisualizerInterface::Execute_OnDrawVisualization(GetOwner());		
+// 	}else {			
+// 		GEngine->AddOnScreenDebugMessage(-1,0, FColor::Red, TEXT("Cannot cast the actor"));
+// 	}		
+// }
---
+++ Source/OnDrawGizmosHook/GizmoComponent.h
@@ -0,0 +1,52 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "ComponentVisualizer.h"
+#include "Components/ActorComponent.h"
+#include "GizmoComponent.generated.h"
+
+
+
+
+
+class FGizmosComponentVisualizer : public FComponentVisualizer {
+public:
+	virtual void DrawVisualization(const UActorComponent* Component, const FSceneView* View, FPrimitiveDrawInterface* PDI) override;
+};
+class UGizmoVisComponent;
+
+DECLARE_DYNAMIC_MULTICAST_SPARSE_DELEGATE(FOnGizmosSelectedDelegate, UGizmoVisComponent, OnGizmosSelected);
+
+
+UCLASS(BlueprintType, Blueprintable)
+class ONDRAWGIZMOSHOOK_API UGizmoVisComponent : public UActorComponent {
+	GENERATED_BODY()
+
+public:	
+	UFUNCTION()
+    virtual void SelectGizmo();
+	
+	UPROPERTY(EditAnywhere, CallInEditor, BlueprintAssignable, BlueprintReadWrite, Category = Visualizer)
+	FOnGizmosSelectedDelegate OnGizmosSelected;
+};
+
+
+
+
+UINTERFACE()
+class UDrawGizmo : public UInterface {
+	GENERATED_BODY()
+};
+class ONDRAWGIZMOSHOOK_API IDrawGizmo {
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+	public:
+	UFUNCTION(BlueprintNativeEvent, CallInEditor, Category = Visualizer)	
+    void OnDrawGizmo();
+
+};
+
